1a1c4dfdec6ccda66233497231fda4ed89103bd5
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2022-12-09 13:23:43 +0530

    Create favorite sequence.cpp

---
+++ favorite sequence.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define pb push_back
+using namespace std;
+         
+int main() {
+         int t;
+         cin >> t;
+         while(t--){
+           int n;
+           cin >> n;
+           int a[n];
+           for(int i = 0 ; i < n ; i++){
+               cin >> a[i];
+           }
+        
+           for(int i = 0; i < n/2 ; i++){
+               cout << a[i] << " " << a[n-i-1] << " ";
+           }
+           if(n%2 != 0){
+               cout << a[n/2];
+           }
+           cout << endl;
+         }
+	return 0;
+} 
